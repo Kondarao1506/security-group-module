0f7fde7e8ef6a88727d81a7371850cc06841a4f1
Kondarao1506 <Kondarao1506@gmail.com>
2026-05-29 12:35:51 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "roboshopsg" {
+  count = length(var.component)
+  name        = "${local.common_name}-${var.component[count.index]}" #roboshop-dev-component-sg 
+  description = "Allow TLS inbound traffic and all outbound traffic ${local.common_name}-${var.component[count.index]}" #roboshop-dev-component-sg
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.common_name}-${var.component[count.index]}" #roboshop-dev-component-sg
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "project" { #roboshop
+  type = string
+}
+
+variable "environment" { #dev
+  type = string
+}
+
+variable "component" {
+  type = list
+}
